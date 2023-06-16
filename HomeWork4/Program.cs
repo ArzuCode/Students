a23cacc8928b53efa0092e58fe4b6422b4dfa964
Arzu.Code <arzuem@code.edu.az>
2023-06-17 02:19:40 +0300

    first commit

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HomeWork4
+{
+    public  class Program
+    {     
+      public  class Student
+        {
+          public string name;
+          
+
+      } 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter total student count:");
+            int StudentCount = int.Parse(Console.ReadLine());
+           
+            Student[] Student = new Student[StudentCount];
+
+            for (int i = 0; i < StudentCount; i++)
+            {
+                Student[i] = new Student();
+                Console.WriteLine("Enter Student name:");
+                Student[i].name = Console.ReadLine();
+                Console.WriteLine("Enter Sudent grade:");
+                int Studentgrade = int.Parse(Console.ReadLine());
+
+                if ( Studentgrade >= 0)
+                {
+                    Console.WriteLine(StatisticsHelper.GetAverageGrade);
+                }
+                if (Studentgrade >=30 )
+                {
+                    Console.WriteLine(StatisticsHelper.GetHighestGrade);
+                }
+                if (Studentgrade >= 50)
+                {
+                    Console.WriteLine(StatisticsHelper.GetLowestGrade);
+                }
+            }
+            
+        }
+  
+    }
+}
---
+++ HomeWork4/StatisticsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    public static class StatisticsHelper
+    {
+        public static void GetAverageGrade()
+            
+        { 
+            Console.WriteLine("Average Grade");
+        }
+        public static void GetHighestGrade()
+        {
+            Console.WriteLine("Highest Grade");
+        }
+        public static void GetLowestGrade()
+        {
+            Console.WriteLine("Lowest Grade");
+        }
+    }  
+      
+}
